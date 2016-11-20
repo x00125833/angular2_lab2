dd18239be4c34e3db4cb35becf27018f2e0eabaf
Enda Lee <enda.lee@ittdublin.ie>
2016-11-20 15:47:19 +0000

    Added Click Me

---
+++ src/app/click-me/click-me.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click-me',
+  templateUrl: './click-me.component.html',
+  styleUrls: ['./click-me.component.css']
+})
+export class ClickMeComponent implements OnInit {
+
+// Declare and initialise a string
+clickMessage: string = "";
+
+  constructor() { }
+
+  // This method handles the button click
+  onClickMe() {
+    // Set the message
+    this.clickMessage = "You clicked the button!"
+  }
+
+  ngOnInit() {
+  }
+
+}
