54f9c71a2b6c6a6597d868877e2a65def8360d2f
Enda Lee <enda.lee@ittdublin.ie>
2016-11-21 21:11:06 +0000

    miles to kilo

--- src/app/mile-kilo/mile-kilo.component.ts
+++ src/app/mile-kilo/mile-kilo.component.ts
@@ -6,9 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mile-kilo.component.css']
 })
 export class MileKiloComponent implements OnInit {
-
+// Declare number and miles to kilo variable
+clickMessage: number;
+milesToKilo: number = 1.69034;
+kiloToMiles: number = 0.62137;
   constructor() { }
 
+  // this method handles the click
+
+onClickMe(myMessage: number){
+  //set the message
+
+  this.clickMessage = myMessage * this.milesToKilo;
+}
+
+onClickMe2(myMessage: number){
+  //set the message
+  
+  this.clickMessage = myMessage * this.kiloToMiles;
+}
   ngOnInit() {
   }
 
